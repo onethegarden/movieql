bc834d76d63e81ac68508c00c1c3609d27fde1b9
onethegarden <onethegarden@gmail.com>
2020-04-13 06:42:24 +0900

    temp

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
-import resolvers from './graphql/resolver';
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-    typeDefs:"graphql/schema.graphql",
-    resolvers
-})
+  typeDefs: "graphql/schema.graphql",
+  resolvers
+});
 
-server.start(()=>console.log("graphql server running"));
+console.log("helllo");
